0c4a8b4daca65076d1e8de3c4799fdddbecc2849
nanalelfe <nargiza.nosirova@mail.utoronto.ca>
2016-06-29 05:10:35 -0400

    Implemented the set of RESTful APIs

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ function onRequest (request, response) {
 		fs.createReadStream(file_path).pipe(response);
 	}
 
+	else if (request.method == 'GET' && extension == '.css') {
+		content_type = "text/css";
+		response.writeHead(200, {"Context-Type": content_type});
+		fs.createReadStream(file_path).pipe(response);
+	} 
+
 	else if (request.method == 'GET' && request.url == "/get_articles") {
 		response.setHeader('Content-Type', 'application/json');
 
@@ -104,8 +110,6 @@ function onRequest (request, response) {
 		response.write(JSON.stringify(dates_urls));
 		response.end();
 
-
-
 	}
 
 	else if (request.method == 'GET' && request.url == "/get_tags"){
@@ -133,6 +137,72 @@ function onRequest (request, response) {
 		response.end();
 
 	}
+
+	else if (request.method == 'GET' && request.url == "/get_details") {
+		response.setHeader('Content-Type', 'application/json');
+		var data = fs.readFileSync("file.js");
+		var obj = JSON.parse(data);
+
+		console.log(obj.num_results);
+		response.write(JSON.stringify({"obj_num" : obj[0].num_results}));
+		response.end();
+
+	}
+
+	else if (request.method == 'GET' && request.url.slice(0, 10) == "/get_index") {
+		response.setHeader('Content-Type', 'application/json');
+		var data = fs.readFileSync("file.js");
+		var obj = JSON.parse(data);
+
+		var urls = require('url').parse(request.url, true);
+		var index = urls.query.index;
+
+		var details = {};
+
+		if (index < obj[0].num_results) {
+			var article = obj[0].results[index];
+			console.log(details);
+			details["section"] = article.section;
+			details["subsection"] = article.subsection;
+			details["title"] = article.title;
+			details["abstract"] = article.abstract;
+			details["byline"] = article.byline;
+			details["published_date"] = article.published_date;
+			details["des_facet"] = article.des_facet;
+		}
+		
+		
+
+		response.write(JSON.stringify(details));
+		response.end();
+	}
+
+	else if (request.method == 'GET' && request.url == "/get_imgs") {
+		response.setHeader('Content-Type', 'application/json');
+		var data = fs.readFileSync("file.js");
+		var obj = JSON.parse(data);
+
+		var placeholder = "http://brucemctague.com/wp-content/uploads/2015/10/cool-stuff.jpeg";
+		var articles = {};
+		obj[0].results.forEach(function (article) {
+			article.multimedia.forEach( function (image) {
+				if (image.format == "thumbLarge") {
+					articles[article.title] = [article.url, image.url];
+				}
+			});
+
+			if (!articles[article.title]) {
+				articles[article.title] = [article.url, placeholder];
+			}
+		});
+
+		articles["placeholder"] = placeholder;
+
+		response.write(JSON.stringify(articles));
+		response.end();
+
+	}
+
 	else {
 		send404Response(response);
 	}
